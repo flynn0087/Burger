3d671d015cca21d5f3c65183240a7ed423a46e60
Ben Flynn <flynn0087@gmail.com>
2021-01-10 11:22:56 -0600

    start of orm built

---
+++ config/orm.js
@@ -0,0 +1,14 @@
+// Import MySQL connection.
+const connection = require("../config/connection.js");
+
+// The below helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
+// ["?", "?", "?"].toString() => "?,?,?";
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+};
