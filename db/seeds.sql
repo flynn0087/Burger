31913bb171fd0a188cd2c66ce06275a528ae2dce
Ben Flynn <flynn0087@gmail.com>
2021-01-10 11:10:57 -0600

    added the schema, seeds and package.json

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom and swiss burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Peanut butter burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
