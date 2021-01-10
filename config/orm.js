3588336c64965815d2fac4f5793fabb3d56c63d7
Ben Flynn <flynn0087@gmail.com>
2021-01-10 12:49:39 -0600

    selectAll function added to orm

--- config/orm.js
+++ config/orm.js
@@ -4,11 +4,47 @@ const connection = require("../config/connection.js");
 // The below helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
 // ["?", "?", "?"].toString() => "?,?,?";
 function printQuestionMarks(num) {
-    var arr = [];
+    let arr = [];
   
-    for (var i = 0; i < num; i++) {
+    for (let i = 0; i < num; i++) {
       arr.push("?");
     }
   
     return arr.toString();
 };
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    let arr = [];
+  
+    // loop through the keys and push the key/value as a string int arr
+    for (let key in ob) {
+      let value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Mushroom and swiss burger => 'Mushroom and swiss burger')
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        // e.g. {name: 'Mushroom and swiss burger'} => ["name='Mushroom and swiss burger'"]
+        // e.g. {devoured: true} => ["devoured=true"]
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+};
+
+// Object for all our SQL statement functions.
+let orm = {
+  selectAll: function(tableInput, cb) {
+      let queryString = "SELECT * FROM " + tableInput + ";";
+      connection.query(queryString, function(err, result) {
+          if(err) {
+              throw err;
+          }
+          cb(result);
+      });
+  },  
+}
